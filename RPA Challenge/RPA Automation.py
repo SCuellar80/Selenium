bca4bb52aac3f05f5f49e0a55fe708350ad1de9e
Severiano <severiano.cuellar@gmail.com>
2025-02-09 22:12:20 -0600

    Third attempt - RPA challenge. Using headless mode + GPU disable. Takes around 7 seconds.

--- RPA Challenge/RPA Automation.py
+++ RPA Challenge/RPA Automation.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # enable headless, comment it to disable it
+chrome_options.add_argument("--disable-gpu")  # Disables GPU (recommended for headless)
 driver = webdriver.Chrome(chrome_options)
 driver.get("https://rpachallenge.com/")
 start = driver.find_element(By.CSS_SELECTOR, "button[class*='btn-large']")
