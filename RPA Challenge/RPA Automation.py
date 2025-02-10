630abd9b46109cd600e0434aa513a6ece2bdd58d
Severiano <severiano.cuellar@gmail.com>
2025-02-09 22:41:18 -0600

    Fourth attempt - RPA challenge. Using headless mode + GPU disable + XPATH + Firefox Takes around 3.9 seconds.

--- RPA Challenge/RPA Automation.py
+++ RPA Challenge/RPA Automation.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from page_excel import ExcelReader
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # enable headless, comment it to disable it
-chrome_options.add_argument("--disable-gpu")  # Disables GPU (recommended for headless)
-driver = webdriver.Chrome(chrome_options)
+firefox_options = Options()
+firefox_options.add_argument("--headless")  # enable headless, comment it to disable it
+firefox_options.add_argument("--disable-gpu")  # Disables GPU (recommended for headless)
+driver = webdriver.Firefox(firefox_options)
 driver.get("https://rpachallenge.com/")
 start = driver.find_element(By.CSS_SELECTOR, "button[class*='btn-large']")
 start.click()
@@ -18,13 +18,6 @@ columnsText = excel_data.pop(0)
 button = driver.find_element(By.CSS_SELECTOR, "input[value*='Submit']")
 
 for row in excel_data:
-    #labelFirstName = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelFirstName']")
-    #labelLastName = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelLastName']")
-    #labelCompanyName = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelCompanyName']")
-    #labelRole = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelRole']")
-    #labelAddress = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelAddress']")
-    #labelEmail = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelEmail']")
-    #labelPhone = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelPhone']")
 
     labelFirstName = driver.find_element(By.XPATH, "//input[@ng-reflect-name='labelFirstName']")
     labelLastName = driver.find_element(By.XPATH, "//input[@ng-reflect-name='labelLastName']")
@@ -43,7 +36,6 @@ for row in excel_data:
     labelPhone.send_keys(row[6])
 
     # After all fields completed, submit it
-
     button.click()
 
 time = driver.find_element(By.CSS_SELECTOR, "div[class*='message2']")
