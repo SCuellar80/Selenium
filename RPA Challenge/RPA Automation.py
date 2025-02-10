32370c8fdb9495c49b71307b60a20db61b0846dd
Severiano <severiano.cuellar@gmail.com>
2025-02-09 22:07:29 -0600

    Second attempt - RPA challenge. Finding all elements first and then all sendkey to all elements. Takes around 12 seconds.

--- RPA Challenge/RPA Automation.py
+++ RPA Challenge/RPA Automation.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from page_excel import ExcelReader
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+#chrome_options.add_argument("--headless")  # enable headless, comment it to disable it
+driver = webdriver.Chrome(chrome_options)
 driver.get("https://rpachallenge.com/")
 start = driver.find_element(By.CSS_SELECTOR, "button[class*='btn-large']")
 start.click()
@@ -10,22 +13,29 @@ start.click()
 excel_reader = ExcelReader()
 excel_data = excel_reader.read_excel_data()
 columnsText = excel_data.pop(0)
-columnsText = [
-    "input[ng-reflect-name='labelFirstName']",
-    "input[ng-reflect-name='labelLastName']",
-    "input[ng-reflect-name='labelCompanyName']",
-    "input[ng-reflect-name='labelRole']",
-    "input[ng-reflect-name='labelAddress']",
-    "input[ng-reflect-name='labelEmail']",
-    "input[ng-reflect-name='labelPhone']"
-]
+
+button = driver.find_element(By.CSS_SELECTOR, "input[value*='Submit']")
+
 for row in excel_data:
-        for column in range(len(columnsText)):
-            label = driver.find_element(By.CSS_SELECTOR, columnsText[column])
-            label.send_keys(row[column])
-        # After all fields completed, submit it
-        button = driver.find_element(By.CSS_SELECTOR, "input[value*='Submit']")
-        button.click()
+    labelFirstName = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelFirstName']")
+    labelLastName = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelLastName']")
+    labelCompanyName = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelCompanyName']")
+    labelRole = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelRole']")
+    labelAddress = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelAddress']")
+    labelEmail = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelEmail']")
+    labelPhone = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelPhone']")
+
+    labelFirstName.send_keys(row[0])
+    labelLastName.send_keys(row[1])
+    labelCompanyName.send_keys(row[2])
+    labelRole.send_keys(row[3])
+    labelAddress.send_keys(row[4])
+    labelEmail.send_keys(row[5])
+    labelPhone.send_keys(row[6])
+
+    # After all fields completed, submit it
+
+    button.click()
 
 time = driver.find_element(By.CSS_SELECTOR, "div[class*='message2']")
 timeText = time.text
