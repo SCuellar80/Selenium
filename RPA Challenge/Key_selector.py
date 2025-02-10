22ae025c5b0b0c11f81df71dba9a36ba1abd02bd
Severiano <severiano.cuellar@gmail.com>
2025-02-09 23:15:08 -0600

    Example of main selector updated

--- RPA Challenge/Key_selector.py
+++ RPA Challenge/Key_selector.py
@@ -1,19 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 driver.get("https://rpachallenge.com/")
 
-# Finds the element which is sibling to the final target element
-# This is done this way because the name of the target changes every time and only this element is constant
-# XPATH is used because the CSS selector can not find texts
-label = driver.find_element(By.XPATH, "//label[text()='Role in Company']")
-
-# finds the sibling which is the final target
-input_element = label.find_element(By.XPATH, "./following-sibling::input")
+input_element = driver.find_element(By.CSS_SELECTOR, "input[ng-reflect-name='labelAddress']")
 
 # To ensure target has been properly selected, writes down a sample
-input_element.send_keys("QA Manager")
+input_element.send_keys("My Address")
 
 driver.quit()
